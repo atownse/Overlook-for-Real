94eb6df5871884c25817f6a3fa918cae8a8c0550
Aaron Townsend <atownse17@gmail.com>
2020-11-06 22:47:36 -0600

    Recreate the testing suite for Customer

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,53 +1,48 @@
 import { expect } from 'chai';
+import sampleBookingData from './sample-booking-data'
+import sampleRoomData from './sample-room-data'
+import sampleUserData from './sample-user-data'
 import Customer from '../src/Customer';
 
-describe('Customer', () => {
-  let customer5, booking1, booking2;
+describe.only('Customer', () => {
+  let customers, rooms, bookings, date, customer;
 
   beforeEach(() => {
-    customer5 = new Customer(5, 'Johnny Appleseed');
-    booking1 = {
-      "id": "5fwrgu4i7k55hl6to",
-      "userID": 5,
-      "date": "2020/02/22",
-      "roomNumber": 13,
-      "roomServiceCharges":[ ]
-    }
-    booking2 = {
-      "id": "5fwrgu4i7k55hl6tw",
-      "userID": 5,
-      "date": "2020/04/01",
-      "roomNumber": 12,
-      "roomServiceCharges": []
-    }
+    rooms = sampleRoomData;
+    bookings = sampleBookingData;
+    customers = sampleUserData;
+    date = '2020/01/24';
+    
+    customer = new Customer(customers[0].id, date, bookings, customers[0].name);
   });
 
   it('should be a function', () => {
     expect(Customer).to.be.a('function');
   });
 
-  it('should be an instance of User', () => {
-    expect(customer5).to.be.an.instanceOf(Customer);
+  it('should be an instance of Customer', () => {
+    expect(customer).to.be.an.instanceOf(Customer);
   });
 
-  it('should have a name', () => {
-    expect(customer5.name).to.equal('Johnny Appleseed');
+  it('should have a property of bookings', () => {
+    expect(customer.bookings.length).to.be.equal(3);
   });
 
-  it('should have an empty array of bookings initially', () => {
-    expect(customer5.bookings).to.deep.equal([]);
+  it('should provide the customer\'s first name', () => {
+    let firstName = customer.provideFirstName();
+
+    expect(firstName).to.equal('Leatha');
   });
 
-  it('should be able to provide the first name of the customer only', () => {
-    let firstName = customer5.provideFirstName();
-
-    expect(firstName).to.equal('Johnny');
+  it('should provide the booked rooms for that customer', () => {
+    let customerBookings = customer.provideBookedRooms(bookings);
+    console.log(customerBookings)
+    expect(customerBookings).to.deep.equal([bookings[0], bookings[3], bookings[4]]);
   });
 
-  it('should be able to book a room and save it to bookings', () => {
-    customer5.bookRoom(booking1);
-    customer5.bookRoom(booking2);
+  it.only('should be able to provide the total amount that user has spent at this hotel', () => {
+    let totalSpent = customer.provideTotalCosts(customer.id, bookings, rooms);
+    expect(totalSpent).to.equal(1189.71)
+  });
 
-    expect(customer5.bookings).to.deep.equal([booking1, booking2]);
-  })
-})
+});
